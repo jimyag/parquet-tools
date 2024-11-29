e084fb05c8bdfcbf95bf205880df913a69647892
jimyag <git@jimyag.com>
2024-11-29 17:09:46 +0800

    feat(diff.go)：添加对系统diff命令的支持，以比较Parquet模式差异

--- cmd/diff.go
+++ cmd/diff.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"bytes"
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/apache/arrow/go/v17/parquet/schema"
 	"github.com/jimyag/log"
@@ -38,6 +40,35 @@ func diffRun(cmd *cobra.Command, args []string) {
 	var buf2 bytes.Buffer
 	schema.PrintSchema(schema1, &buf1, 2)
 	schema.PrintSchema(schema2, &buf2, 2)
+	// 如果系统有 diff 命令，则使用系统 diff 命令
+	// 将结果写到临时文件中，然后使用 diff 命令比较
+	if _, err := exec.LookPath("diff"); err == nil {
+		tmp1, err := os.CreateTemp("", "parquet-diff-1.txt")
+		if err != nil {
+			log.Error(err).Msg("error creating temp file")
+			return
+		}
+		defer os.Remove(tmp1.Name())
+		if _, err = tmp1.Write(buf1.Bytes()); err != nil {
+			log.Error(err).Msg("error writing to temp file")
+			return
+		}
+		tmp2, err := os.CreateTemp("", "parquet-diff-2.txt")
+		if err != nil {
+			log.Error(err).Msg("error creating temp file")
+			return
+		}
+		defer os.Remove(tmp2.Name())
+		if _, err = tmp2.Write(buf2.Bytes()); err != nil {
+			log.Error(err).Msg("error writing to temp file")
+			return
+		}
+		cmd := exec.Command("diff", "-u", tmp1.Name(), tmp2.Name())
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+		return
+	}
 	dmp := diffmatchpatch.New()
 	diffs := dmp.DiffMain(buf1.String(), buf2.String(), true)
 	fmt.Println(dmp.DiffPrettyText(diffs))
